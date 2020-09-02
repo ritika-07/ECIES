934fab58cf8bfca057e677ccea8d8ee4646d34c6
ritika-07 <ritikakayal07@gmail.com>
2020-09-02 20:48:20 +0530

    spacy fix

--- spacy_summarization.py
+++ spacy_summarization.py
@@ -1,5 +1,5 @@
 # NLP Pkgs
-import spacy
+import spacy 
 nlp = spacy.load('en')
 # Pkgs for Normalizing Text
 from spacy.lang.en.stop_words import STOP_WORDS
@@ -14,8 +14,8 @@ def text_summarizer(raw_docx):
     docx = nlp(raw_text)
     stopwords = list(STOP_WORDS)
     # Build Word Frequency # word.text is tokenization in spacy
-    word_frequencies = {}
-    for word in docx:
+    word_frequencies = {}  
+    for word in docx:  
         if word.text not in stopwords:
             if word.text not in word_frequencies.keys():
                 word_frequencies[word.text] = 1
@@ -23,16 +23,16 @@ def text_summarizer(raw_docx):
                 word_frequencies[word.text] += 1
 
 
-maximum_frequncy = max(word_frequencies.values())
+    maximum_frequncy = max(word_frequencies.values())
 
-for word in word_frequencies.keys():
-    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+    for word in word_frequencies.keys():  
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
     # Sentence Tokens
     sentence_list = [ sentence for sentence in docx.sents ]
-    
+
     # Sentence Scores
-    sentence_scores = {}
-    for sent in sentence_list:
+    sentence_scores = {}  
+    for sent in sentence_list:  
         for word in sent:
             if word.text.lower() in word_frequencies.keys():
                 if len(sent.text.split(' ')) < 30:
@@ -42,7 +42,7 @@ for word in word_frequencies.keys():
                         sentence_scores[sent] += word_frequencies[word.text.lower()]
 
 
-summarized_sentences = nlargest(7, sentence_scores, key=sentence_scores.get)
-final_sentences = [ w.text for w in summarized_sentences ]
-summary = ' '.join(final_sentences)
-return summary
+    summarized_sentences = nlargest(7, sentence_scores, key=sentence_scores.get)
+    final_sentences = [ w.text for w in summarized_sentences ]
+    summary = ' '.join(final_sentences)
+    return summary
